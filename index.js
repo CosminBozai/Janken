aa8e3a1974ab7e60e1851968b68ce05fef0ca4e4
CosminBozai <cosminbozai@outlook.com>
2022-07-13 14:38:28 +0100

    Made the logic for player selection buttons

--- index.js
+++ index.js
@@ -5,32 +5,27 @@ let playerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
-//Function that keeps the game running for a number of rounds
-function game() {
-  for (let i = 0; i < 5; i++) {
-    computerSelection =
-      computerOptions[Math.floor(Math.random(computerOptions) * 3)];
-    console.log(computerSelection);
-    playerSelection = prompt().toLowerCase();
-    console.log(playerSelection);
-    playRound(playerSelection, computerSelection);
-    console.log(`You: ${playerScore}`, `Computer: ${computerScore}`);
-  }
-  console.log("The final score is:");
-  console.log(`You: ${playerScore}`);
-  console.log(`Computer: ${computerScore}`);
-  if (playerScore > computerScore) {
-    console.log(
-      "You beat the computer! You must think you're pretty smart now."
-    );
-  } else {
-    console.log("You lose! Better luck next time.");
-  }
-}
-game();
+const rock = document.getElementById("rock");
+rock.addEventListener("click", function () {
+  playRound("rock");
+});
+
+const paper = document.getElementById("paper");
+paper.addEventListener("click", function () {
+  playRound("paper");
+});
+
+const scissors = document.getElementById("scissors");
+scissors.addEventListener("click", function () {
+  playRound("scissors");
+});
 
 //Play one round
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  computerSelection =
+    computerOptions[Math.floor(Math.random(computerOptions) * 3)];
+  console.log(playerSelection);
+  console.log(computerSelection);
   if (playerSelection === computerSelection) {
     console.log("Tie");
   } else if (playerSelection == "rock") {
